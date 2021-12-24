e2106d85d45f220d735cca17ec4d4cfbbdef2606
Sail Somoshi <sailsomoshi11@gmail.com>
2021-12-24 22:39:47 +0530

    ADDED: Roll die

--- SnakeLader.java
+++ SnakeLader.java
@@ -5,5 +5,7 @@ public class SnakeLader {
 		System.out.println("Welcome to snake ladder game");
 		int start = 0;
 		System.out.println("Starting position of player is: " + start);
+		int rollDie = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+		System.out.println(rollDie);
 	}
 }
