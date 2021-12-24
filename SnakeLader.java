a7a8a245ca055fb2e9cc5b50155e0016e8bc8976
Sail Somoshi <sailsomoshi11@gmail.com>
2021-12-24 22:34:23 +0530

    Start position

--- SnakeLader.java
+++ SnakeLader.java
@@ -3,5 +3,7 @@ package snakeLadder;
 public class SnakeLader {
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake ladder game");
+		int start = 0;
+		System.out.println("Starting position of player is: " + start);
 	}
 }
