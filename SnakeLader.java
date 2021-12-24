24488300b3d29bf5048bddca4d79e28b78f7e8cf
Sail Somoshi <sailsomoshi11@gmail.com>
2021-12-24 23:36:00 +0530

    ADDED; Repeat till the player reach winning position 100

--- SnakeLader.java
+++ SnakeLader.java
@@ -5,37 +5,39 @@ public class SnakeLader {
 	public static final int LADDER = 1;
 	public static final int SNAKE = 2;
 	public static final int START = 0;
-	public static void main(String[] args) {
-		System.out.println("Welcome to snake ladder game");
-		int start = 0;
-		System.out.println("Starting position of player is: " + start);
-		int rollDie = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-		System.out.println(rollDie);
-		int pos = 0;
-		System.out.println("Number on rolled dice is : " +rollDie);
-		int check = (int) (Math.floor(Math.random() * 10) % 3);
-		System.out.println("Condition: " +check);
-	switch (check){
-	case NO_PLAY:
-		System.out.println("New Position is: " + pos);
-	break;
-	case LADDER:
-		System.out.println("you get LADDER");
-        pos = pos + rollDie;
-        System.out.println("New Position is: " + pos);
-        break;
-	case SNAKE:
-		System.out.println("you got snake");
-        pos = pos - rollDie;
-        if ( pos >= 0)
-        	 System.out.println("New Position is: " + pos);
-		else
-			 pos = 0;
-		     System.out.println("New Position is: " + pos);
-	break;
-	default:
-		System.out.println("Default");
-		break;
-	}
-  }
+	public static void main(String[] args){
+	int position = 0;
+    System.out.println("Starting position of player is: " + START);
+    while (position <= 100){
+    int rollDie = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+    System.out.println("Number on rolled dice is : " +rollDie);
+    int check = (int) (Math.floor(Math.random() * 10) % 3);
+    System.out.println("Condition: " +check);
+    switch (check){
+          case NO_PLAY:
+                System.out.println("New Position is: " + position);
+          break;
+          case LADDER:
+                position = position + rollDie;
+                System.out.println("New Position is: " + position);
+          break;
+          case SNAKE:
+                position = position - rollDie;
+                if (position >= 0 )
+                	System.out.println("New Position is: " + position);
+                else
+                	position = 0;
+                	System.out.println("New Position is: " + position);
+          break;
+          default:
+                System.out.println("Default");
+                break;
+       }
+    }
+    System.out.println();
+    if (position >= 100)
+    System.out.println("-------------------");
+    System.out.println("    PLAYER WON");
+    System.out.println("-------------------");
+    }
 }
